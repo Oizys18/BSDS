2e8499f7059e7f7231fbf51f7e967c85539d7fa3
양찬우 <oizys18@gmail.com>
2020-04-21 20:31:07 +0900

    Add navbar on user_index

---
+++ front/src/views/user/components/navbar.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="navbar-container">
+    <h1>분실둥실</h1>
+    <div id="navbtn-wrapper">
+      <buttonDefault :text="btnText" />
+      <buttonDefault :text="btnText2" />
+      <buttonDefault :text="btnText3" />
+    </div>
+  </div>
+</template>
+
+<script>
+import buttonDefault from "@/components/common/button/buttonDefault.vue";
+export default {
+  name: "navbar",
+  data() {
+    return {
+      btnText: "분실물 등록",
+      btnText2: "분실물 목록",
+      btnText3: "분실신고조회",
+    };
+  },
+  components: {
+    buttonDefault,
+  },
+};
+</script>
+
+<style scoped>
+.navbar-container {
+  /* position */
+  position: absolute;
+  top: 0;
+  left: 0;
+
+  /* shape and style */
+  height: 50px;
+  width: 100%;
+  background-color: whitesmoke;
+  box-shadow: 0 0 5px;
+
+  /* content align */
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+#navbtn-wrapper{
+    position: absolute;
+    right:0;
+}
+</style>
--- front/src/views/user/user.vue
+++ front/src/views/user/user.vue
@@ -1,15 +1,25 @@
 <template>
   <div>
-    <h1>UserIndex</h1>
+    <navbar/>
+    <div id="user-index-container">
+      <h1>hello</h1>
+    </div>
   </div>
 </template>
 
 <script>
+import navbar from "./components/navbar";
 export default {
-  name:'userIndex'
+  name:'userIndex',
+  components:{
+    navbar
+  }
 }
 </script>
 
 <style>
-
+#user-index-container{
+  width:100%;
+  margin:50px, 10%, 0, 10%;
+}
 </style>
