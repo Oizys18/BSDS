fa85ef311c7f1bf72fdf92ecdb3b067b596cf197
조현동 <jay.hyundong@gmail.com>
2020-04-21 19:00:59 +0900

    Add database/get_image/

--- database/get_image/public_api.py
+++ database/get_image/public_api.py
@@ -1,11 +1,7 @@
 from bs4 import BeautifulSoup
 from decouple import config
-import json
-from collections import OrderedDict
-from urllib.parse import quote
 from urllib.request import urlopen
 import csv
-import os
 
 
 def save_as_csv(data):
@@ -17,34 +13,42 @@ def save_as_csv(data):
             imgFilePath = dt['imgFilePath']
             hiPrdtNm = dt['hiPrdtNm']
             prdtNm = dt['prdtNm']
-            w.writerow([atcId, imgFilePath, hiPrdtNm, prdtNm])
-
-    # with open("img_path.txt", 'a') as f:
-    #     for dt in data:
-    #         img = f"{dt['atcId']},{dt['imgFilePath']},{dt['hiPrdtNm']},{dt['prdtNm']}\n"
-    #         f.write(img)
+            color_eng = dt['color_eng']
+            color_kor = dt['color_kor']
+            w.writerow([atcId, imgFilePath, hiPrdtNm, prdtNm, color_eng, color_kor])
 
 
 def get_picture(items):
-    # global name_db
+    global color_map
+
     db = []
     for item in items:
         imgFilePath = item.select_one('fdFilePathImg').text
-        if '_no_img' not in imgFilePath:
+        if '_no_img' not in imgFilePath and 'noimg' not in imgFilePath:
             prdtClNm = item.select_one('prdtClNm').text
             atcId = item.select_one('atcId').text
             temp = prdtClNm.split(' > ')
             hiPrdtNm, prdtNm = temp[0], temp[1]
+            fdSbjt = item.select_one('fdSbjt').text
 
-            # if not name_db.get(hiPrdtNm):
-            #     name_db[hiPrdtNm] = set()
-            # name_db[hiPrdtNm].add(prdtNm)
+            fdSbjt = fdSbjt.split('색)')[-2]
+            fdSbjt = fdSbjt.split('(')
+            color_eng, color_kor = fdSbjt[-2][:], fdSbjt[-1][:-1]
+
+            if color_map.get(color_eng):
+                if not color_map[color_eng] == color_kor:
+                    print(color_eng, color_kor)
+                    color_kor = color_map[color_eng]
+            else:
+                color_map[color_eng] = color_kor
 
             db.append({
                 'atcId': atcId,
                 'imgFilePath': imgFilePath,
                 'hiPrdtNm': hiPrdtNm,
-                'prdtNm': prdtNm
+                'prdtNm': prdtNm,
+                'color_eng': color_eng,
+                'color_kor': color_kor,
             })
 
     return db
@@ -52,13 +56,14 @@ def get_picture(items):
 
 ServiceKey = config('ServiceKey')
 pageNo = 1
-numOfRows = 50
+numOfRows = 200
 
-# name_db = {}
-real_db = []
-for pageNo in range(1, 3):
-    print()
-    print('============ pageNo: ', pageNo, ' ============')
+color_map = {}
+for pageNo in range(1, 421):
+
+    if not pageNo % 10:
+        print('============ pageNo: ', pageNo, ' ============')
+
     params = f'ServiceKey={ServiceKey}&pageNo={pageNo}&numOfRows={numOfRows}'
     URL = f'http://apis.data.go.kr/1320000/LosPtfundInfoInqireService/getPtLosfundInfoAccTpNmCstdyPlace?' + params
     res = urlopen(URL)
@@ -66,18 +71,15 @@ for pageNo in range(1, 3):
     if soup.select_one('header > resultCode').text == '00':
         db = get_picture(soup.select_one('body > items'))
         save_as_csv(db)
-        real_db += db
+    else:
+        print(pageNo)
 
-with open('lost_images.json', 'w', encoding="utf-8") as f:
-    json.dump(real_db, f, ensure_ascii=False, indent='\t')
+colors = []
+for key, value in color_map.items():
+    colors.append((key, value))
 
+with open('color_map.csv', 'w', newline='', encoding='utf-8') as f:
+    w = csv.writer(f, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
 
-# name_list = []
-# for key, value in name_db.items():
-#     name_list.append({
-#         'hiPrdtNm': key,
-#         'prdtNm': list(value)
-#     })
-#
-# with open('prdtNm_key.json', 'w', encoding='utf-8') as f:
-#     json.dump(name_list, f, ensure_ascii=False, indent='\t')
+    for idx, (eng, kor) in enumerate(colors):
+        w.writerow([idx + 1, eng, kor])
