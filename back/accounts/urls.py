4f93a507d6960971b56d1d9693003f33ef719d0c
조현동 <jay.hyundong@gmail.com>
2020-04-23 16:50:17 +0900

    Add GET user_detail

--- back/accounts/urls.py
+++ back/accounts/urls.py
@@ -1,8 +1,22 @@
 from django.urls import path
 from . import views
 
-app_name = 'accounts'
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+
+schema_view = get_schema_view(
+    openapi.Info(
+        #
+        title="LostAndFound API",
+        default_version="v1",
+        #
+        description='아자아자',
+        contact=openapi.Contact(email='jay.hyundong@gmail.com'),
+        license=openapi.License(name='SSAFY License'),
+    )
+)
 
 urlpatterns = [
-
+    path('swagger/', schema_view.with_ui('swagger'), name='api_swagger'),
+    path('<int:user_id>/', views.user_detail, name='user_detail'),
 ]
--- back/accounts/views.py
+++ back/accounts/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, HttpResponse
+from .models import User, Address
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-# Create your views here.
+import json
+
+
+@api_view(['GET'])
+def user_detail(request, user_id):
+    user = get_object_or_404(User, id=user_id)
+    address = get_object_or_404(Address, id=user_id)
+
+    dataset = {
+        'id': user_id,
+        'center_name': f'{user.center_name} {user.role}',
+        'parent_department': user.parent_department,
+        'phone_number': user.phone_number,
+        'address': address.address_name,
+    }
+
+    res_data = json.dumps(dataset, ensure_ascii=False)
+    return Response(status=200, data=res_data, content_type='application/json')
+
+
