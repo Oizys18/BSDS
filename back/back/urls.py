ee14e6d939b47511b4c67a888e1370c2eaa67c88
조현동 <jay.hyundong@gmail.com>
2020-04-23 12:31:15 +0900

    Add accouts/urls.py, lost/urls.py

--- back/back/urls.py
+++ back/back/urls.py
@@ -19,6 +19,8 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    path('images/', include('images.urls')),
+    # path('images/', include('images.urls')),
     path('found/', include('found.urls')),
+    path('lost/', include('lost.urls')),
+    path('auth/', include('accounts.urls')),
 ]
