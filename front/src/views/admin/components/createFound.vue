468da71b90e7f0fd9833f2bf21364d24d0f2e30c
최솔지 <bssj9307@gmail.com>
2020-04-24 09:10:40 +0900

    Add createFound S02P23A405-69

---
+++ front/src/views/admin/components/createFound.vue
@@ -0,0 +1,218 @@
+<template>
+<div class="container">
+  <navbar/>
+  <div class="create-lost">
+    <form @submit.prevent="createContent">
+      <div class=left-wrapper>
+        <div class="img-wrapper">
+          <label for="file-input" class="image-label">
+            <img class="image-upload" src="@/assets/images/camera.png">
+          </label>
+          <input
+            class="file-input"
+            id="file-input"
+            ref="imageInput"
+            type="file"
+            accept="image/*"
+            @change="onChangeImages"
+          >
+          <img class="image-preview" v-if="imageUrl" :src="imageUrl"/>
+        </div>
+        <div class="category-wrapper">
+          <span class="select">물품 분류</span>
+          <select-one
+            class="select-category"
+            :default="'분류'"
+          />
+        </div>
+        <div class="input-wrapper">
+            <textarea
+              class="content-area"
+              v-model="contents"
+              type="textarea"
+              placeholder="필요한 내용이 있다면 입력해주세요."
+              @input="onChangeInput"
+            />
+        </div>
+      </div>
+      <div class="right-wrapper">
+      <span @click="go('created')">
+      <button-default :text="'등록하기'"/>
+      </span>
+      </div>
+    </form>
+  </div>
+</div>
+</template>
+
+<script>
+import selectOne from '@/components/common/dropdown/selectOne'
+import navbar from '@/views/user/components/navbar'
+import buttonDefault from '@/components/common/button/buttonDefault'
+const axios = require('axios')
+
+export default {
+  name: 'create-lost',
+  components: {
+    selectOne,
+    navbar,
+    buttonDefault
+  },
+  data () {
+    return {
+      image: '',
+      contents: '',
+      imageUrl: null,
+      yItems: [],
+      mItems: ['1 월', '2 월', '3 월','4 월','5 월','6 월','7 월','8 월','9 월','10 월','11 월','12 월'],
+      dItems: [],
+      date: ['2020', '-', '00', '-', '00'],
+      timeList: [],
+      startTime: '',
+      endTime: '',
+    }
+  },
+  mounted () {
+    for (let i=0; i<25; i++) {
+      if (i<10) {
+          this.timeList.push('0' + i.toString() + ':00') 
+      } else {
+          this.timeList.push(i.toString() + ':00') 
+      }
+    }
+    for (let y=2020; y>1920; y--) {
+      this.yItems.push(y.toString() + ' 년')
+    }
+    for (let d=1; d<32; d++) {
+      this.dItems.push(d.toString() + ' 일')
+    }
+  },
+  methods: {
+    createContent () {
+      const formdata = {
+        "contents": this.contents,
+        "imageFile": this.image,
+        "date": this.date.reduce(function (accumulator, currentValue) {
+          return accumulator + currentValue;
+        }),
+        "time": this.startTime.replace(":", "") + this.endTime.replace(":", "")
+      }
+      axios.post('http://localhost:3001/board', this.formdata, {
+        headers: {
+            "Content-Type": "multipart/form-data",
+        }
+      })
+      .then(res => {
+        console.log(res)
+        console.log(formdata)
+      })
+      .catch(err => {
+        console.log(err)
+      })
+    },
+    onClickImageUpload() {
+      this.$refs.imageInput.click();
+    },
+    onChangeImages(e) {
+      console.log(e.target.files)
+      const file = e.target.files[0] 
+      this.image = file
+      this.imageUrl = URL.createObjectURL(file) 
+      console.log(this.image)
+      // 이미지 post 한번 더 보내서 분류 추가할것
+    },
+    onChangeInput() {
+      console.log(this.contents)
+    },
+    onTimeSelectStart(value) {
+      this.startTime = this.timeList[value]
+      console.log(this.startTime)
+    },
+    onTimeSelectEnd(value) {
+      this.endTime = this.timeList[value]
+    },
+    onDateSelectY(value) {
+      this.date[0] = this.yItems[value].slice(0, -2)
+      console.log(this.date)
+    },
+    onDateSelectM(value) {
+      var temp = this.mItems[value].slice(0, -2)
+      if (temp < 10) {
+        this.date[2] = '0' + temp
+      } else {
+        this.date[2] = temp
+      }
+      console.log(this.date)
+    },
+    onDateSelectD(value) {
+      var temp = this.dItems[value].slice(0, -2)
+      if (temp < 10) {
+        this.date[4] = '0' + temp
+      } else {
+        this.date[4] = temp
+      }
+      console.log(this.date)
+    },
+    go(path) {
+      this.$router.push(path);
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .content-area {
+    width: 80%;
+    height: 100px;
+    margin: 5px;
+    padding: 10px;
+    -webkit-appearance: none;
+    -moz-appearance: none;
+    appearance: none;
+  }
+  .file-input {
+    display: none;
+  }
+  .container {
+    margin-top: 150px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+  .create-form {
+    overflow: hidden;
+  }
+  .left-wrapper {
+    float: left;
+    width: 50em;
+    border: 1px solid black;
+    margin-right: 3em;
+    margin-bottom: 2em;
+    padding: 1rem;
+  }
+  .right-wrapper {
+    border: 1px solid black;
+    float: left;
+    width: 20em;
+    padding: 10px;
+  }
+  .img-wrapper {
+    margin: 5px; 
+  }
+  .category-wrapper, .input-wrapper {
+    display: flex;
+    margin-bottom: 15px;
+  }
+  .image-preview {
+    width: 200px;
+    height: 150px;
+  }
+  .image-upload {
+    margin-right: 20px;
+  }
+  .select {
+    font-size: 1.1rem;
+    font-weight: bold;
+    margin: 0 15px 0 10px;
+  }
+</style>
