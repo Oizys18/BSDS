038e1c27b2fb7784f83df4f9d761bc6132575271
김수민 <mytoutbon@gmail.com>
2020-04-29 15:33:46 +0900

    Mod back/found/views.py, back/ai/views.py
    Former-commit-id: 81e8de31ce8e807ad65f4452ba6b47e206583ed0

--- back/ai/views.py
+++ back/ai/views.py
@@ -179,6 +179,7 @@ def get_hex(colorData):
 
 def hex_to_rgb(value):
     value = value.lstrip('#')
+    print(value)
     lv = len(value)
     return tuple(int(value[i:i + lv // 3], 16) for i in range(0, lv, lv // 3))
 
